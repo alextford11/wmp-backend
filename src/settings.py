1fcae2aa035725592f325221a7c13c038358ff70
Alex Ford <alextford1@gmail.com>
2022-03-31 21:25:59 +0100

    database url change for heroku

--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,15 @@
 from typing import Optional
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 
 class Settings(BaseSettings):
     dev_mode: bool = False
     pg_dsn: Optional[str] = 'postgresql://postgres@localhost:5432/wmp_backend'
+
+    @validator('pg_dsn')
+    def heroku_ready_pg_dsn(cls, v):
+        return v.replace('gres://', 'gresql://')
 
     class Config:
         fields = {
